1813c6338f3630b9c5c38ab0e0c1ebbb54b3a25a
sirate <47669387+sirate@users.noreply.github.com>
2021-05-17 21:31:47 +0100

    Add files via upload

---
+++ ServiceAvis.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.services;
+
+import com.codename1.io.CharArrayReader;
+import com.codename1.io.ConnectionRequest;
+import com.codename1.io.JSONParser;
+import com.codename1.io.NetworkEvent;
+import com.codename1.io.NetworkManager;
+import com.codename1.ui.events.ActionListener;
+import com.mycompany.myapp.entities.Avis;
+import com.mycompany.myapp.entities.Categorie;
+import static com.mycompany.myapp.services.ServiceAvis.instance;
+import com.mycompany.myapp.utils.Statics;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author THINKPAD
+ */
+public class ServiceAvis {
+          
+    public ArrayList<Avis> Avis;
+    public boolean resultOK;
+    public static ServiceAvis instance=null;
+        private ConnectionRequest req;
+     public ServiceAvis() {
+         req = new ConnectionRequest();
+    }
+      public static ServiceAvis getInstance() {
+        if (instance == null) {
+            instance = new ServiceAvis();
+        }
+        return instance;
+    }
+ 
+     public ArrayList<Avis> parseAvis(String jsonText){
+        try {
+           Avis=new ArrayList<>();
+            JSONParser j = new JSONParser();// Instanciation d'un objet JSONParser permettant le parsing du résultat json
+
+            /*
+                On doit convertir notre réponse texte en CharArray à fin de
+            permettre au JSONParser de la lire et la manipuler d'ou vient 
+            l'utilité de new CharArrayReader(json.toCharArray())
+            
+            La méthode parse json retourne une MAP<String,Object> ou String est 
+            la clé principale de notre résultat.
+            Dans notre cas la clé principale n'est pas définie cela ne veux pas
+            dire qu'elle est manquante mais plutôt gardée à la valeur par defaut
+            qui est root.
+            En fait c'est la clé de l'objet qui englobe la totalité des objets 
+                    c'est la clé définissant le tableau de tâches.
+            */
+            Map<String,Object> AvisListJson = j.parseJSON(new CharArrayReader(jsonText.toCharArray()));
+            
+              /* Ici on récupère l'objet contenant notre liste dans une liste 
+            d'objets json List<MAP<String,Object>> ou chaque Map est une tâche.               
+            
+            Le format Json impose que l'objet soit définit sous forme
+            de clé valeur avec la valeur elle même peut être un objet Json.
+            Pour cela on utilise la structure Map comme elle est la structure la
+            plus adéquate en Java pour stocker des couples Key/Value.
+            
+            Pour le cas d'un tableau (Json Array) contenant plusieurs objets
+            sa valeur est une liste d'objets Json, donc une liste de Map
+            */
+            List<Map<String,Object>> list = (List<Map<String,Object>>)AvisListJson.get("root");
+            
+            //Parcourir la liste des tâches Json
+            for(Map<String,Object> obj : list){
+                //Création des tâches et récupération de leurs données
+               Avis a = new Avis();
+               float id = Float.parseFloat(obj.get("id").toString());
+                a.setId((int)id);
+                a.setNote(((int)Float.parseFloat(obj.get("note").toString())));
+             
+             
+                Avis.add(a);
+            }
+            
+            
+        } catch (IOException ex) {
+            
+        }
+         /*
+            A ce niveau on a pu récupérer une liste des tâches à partir
+        de la base de données à travers un service web
+        
+        */
+        return  Avis;
+    }
+      public ArrayList<Avis> getAllAvis(){
+         String url = Statics.BASE_URL + "/avismob";
+        req.setUrl(url);
+        req.setPost(false);
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+                Avis = parseAvis (new String(req.getResponseData()));
+                req.removeResponseListener(this);
+            }
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return Avis ;
+    }
+    
+    public boolean  Delete(int id){
+       String url = Statics.BASE_URL + "/supprimerAvismobile/" +id;
+
+        req.setUrl(url);
+
+        req.addResponseListener(new ActionListener<NetworkEvent>() {
+            @Override
+            public void actionPerformed(NetworkEvent evt) {
+               
+                req.removeResponseListener(this);
+            }
+
+        });
+        NetworkManager.getInstance().addToQueueAndWait(req);
+        return resultOK;
+      
+      
+      }
+}
